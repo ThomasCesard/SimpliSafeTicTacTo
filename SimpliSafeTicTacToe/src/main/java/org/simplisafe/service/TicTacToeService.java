ce41b83e2f805e64fc98881d5e73865aeb68d2ea
Tommy <108021640+ThomasCesard@users.noreply.github.com>
2024-07-23 17:02:13 -0400

    Add files via upload

--- SimpliSafeTicTacToe/src/main/java/org/simplisafe/service/TicTacToeService.java
+++ SimpliSafeTicTacToe/src/main/java/org/simplisafe/service/TicTacToeService.java
@@ -16,22 +16,26 @@ public class TicTacToeService {
      * also a winner if the same tile is in all four corners,
      * or in a 2x2 box on the board.
      *
-     * @param board a valid 4x4 tic-tac-toe board.
+     * @param board a valid tic-tac-toe board.
      * @return the Tile of the winner, or an empty Tile if there is no winner.
      */
     public Tile getWinner(TicTacToeBoard board) {
-        Tile winner = checkCorners(board.getTiles());
+        if (board.getBoardSize() <= 0) {
+            return Tile.E;
+        }
+
+        Tile winner = checkCorners(board);
         if (Tile.E.equals(winner)) {
-            winner = checkDiagonals(board.getTiles());
+            winner = checkDiagonals(board);
         }
         if (Tile.E.equals(winner)) {
-            winner = checkVerticals(board.getTiles());
+            winner = checkVerticals(board);
         }
         if (Tile.E.equals(winner)) {
-            winner = checkHorizontals(board.getTiles());
+            winner = checkHorizontals(board);
         }
         if (Tile.E.equals(winner)) {
-            winner = checkBoxes(board.getTiles());
+            winner = checkBoxes(board);
         }
         return winner;
     }
@@ -41,7 +45,7 @@ public class TicTacToeService {
      * A valid move can be made if there is an empty tile on the board,
      * regardless if the game has been won or not.
      *
-     * @param board a valid 4x4 tic-tac-toe board.
+     * @param board a valid tic-tac-toe board.
      * @return true if a move can be made, false otherwise.
      */
     public boolean canMove(TicTacToeBoard board) {
@@ -54,70 +58,78 @@ public class TicTacToeService {
      * Compute if a game of tic-tac-toe is over. A game is over
      * if there are no more valid moves, or if there is a winner.
      *
-     * @param board a valid 4x4 tic-tac-toe board.
+     * @param board a valid tic-tac-toe board.
      * @return true if the game is over, false otherwise.
      */
     public boolean isGameOver(TicTacToeBoard board) {
         return !canMove(board) || !Tile.E.equals(getWinner(board));
     }
 
-    private Tile checkCorners (List<List<Tile>> tiles) {
+    private Tile checkCorners (TicTacToeBoard board) {
+        int boardSize = board.getBoardSize();
+        List<List<Tile>> tiles = board.getTiles();
         Tile firstTile = tiles.get(0).get(0);
+
         boolean isWinner = !Tile.E.equals(firstTile)
-                && firstTile.equals(tiles.get(0).get(3))
-                && firstTile.equals(tiles.get(3).get(0))
-                && firstTile.equals(tiles.get(3).get(3));
+                && firstTile.equals(tiles.get(0).get(boardSize - 1))
+                && firstTile.equals(tiles.get(boardSize - 1).get(0))
+                && firstTile.equals(tiles.get(boardSize - 1).get(boardSize - 1));
         return isWinner ? firstTile : Tile.E;
     }
 
-    private Tile checkDiagonals(List<List<Tile>> tiles) {
-        Tile upperLeftTile = tiles.get(0).get(0);
-        Tile upperRightTile = tiles.get(0).get(3);
-
-        boolean upperLeftWinner = !Tile.E.equals(upperLeftTile)
-                && upperLeftTile.equals(tiles.get(1).get(1))
-                && upperLeftTile.equals(tiles.get(2).get(2))
-                && upperLeftTile.equals(tiles.get(3).get(3));
-        if (upperLeftWinner) {
-            return upperLeftTile;
-        }
-
-        boolean upperRightWinner = !Tile.E.equals(upperRightTile)
-                && upperRightTile.equals(tiles.get(1).get(2))
-                && upperRightTile.equals(tiles.get(2).get(1))
-                && upperRightTile.equals(tiles.get(3).get(0));
-        if (upperRightWinner) {
-            return upperRightTile;
-        }
-
-        return Tile.E;
+    private Tile checkDiagonals(TicTacToeBoard board) {
+        Tile upperLeftTile = checkUpperLeftDiagonal(board);
+        return !Tile.E.equals(upperLeftTile) ? upperLeftTile : checkUpperRightDiagonal(board);
     }
 
-    private Tile checkHorizontals(List<List<Tile>> tiles) {
-        return IntStream.range(0, 4)
+    private Tile checkUpperLeftDiagonal(TicTacToeBoard board) {
+        List<List<Tile>> tiles = board.getTiles();
+        Tile upperLeftTile = tiles.get(0).get(0);
+
+        boolean upperLeftWinner = !Tile.E.equals(upperLeftTile) && IntStream.range(1, board.getBoardSize())
+                .allMatch(i -> upperLeftTile.equals(tiles.get(i).get(i)));
+
+        return upperLeftWinner ? upperLeftTile : Tile.E;
+    }
+
+    private Tile checkUpperRightDiagonal(TicTacToeBoard board) {
+        List<List<Tile>> tiles = board.getTiles();
+        Tile upperRightTile = tiles.get(0).get(board.getBoardSize() - 1);
+
+        boolean upperRightWinner = !Tile.E.equals(upperRightTile) && IntStream.range(1, board.getBoardSize())
+                .allMatch(i -> upperRightTile.equals(tiles.get(i).get(board.getBoardSize() - 1 - i)));
+
+        return upperRightWinner ? upperRightTile : Tile.E;
+    }
+
+    private Tile checkHorizontals(TicTacToeBoard board) {
+        List<List<Tile>> tiles = board.getTiles();
+        return IntStream.range(0, board.getBoardSize())
                 .filter(i -> {
                     Tile firstTile = tiles.get(i).get(0);
-                    return !Tile.E.equals(firstTile) && IntStream.range(1, 4)
+                    return !Tile.E.equals(firstTile) && IntStream.range(1, board.getBoardSize())
                             .allMatch(j -> firstTile.equals(tiles.get(i).get(j)));
                 })
                 .mapToObj(i -> tiles.get(i).get(0))
                 .findAny().orElse(Tile.E);
     }
 
-    private Tile checkVerticals(List<List<Tile>> tiles) {
-        return IntStream.range(0, 4)
+    private Tile checkVerticals(TicTacToeBoard board) {
+        List<List<Tile>> tiles = board.getTiles();
+        return IntStream.range(0, board.getBoardSize())
                 .filter(i -> {
                     Tile firstTile = tiles.get(0).get(i);
-                    return !Tile.E.equals(firstTile) && IntStream.range(1, 4)
+                    return !Tile.E.equals(firstTile) && IntStream.range(1, board.getBoardSize())
                             .allMatch(j -> firstTile.equals(tiles.get(j).get(i)));
                 })
                 .mapToObj(i -> tiles.get(0).get(i))
                 .findAny().orElse(Tile.E);
     }
 
-    private Tile checkBoxes(List<List<Tile>> tiles) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+    private Tile checkBoxes(TicTacToeBoard board) {
+        List<List<Tile>> tiles = board.getTiles();
+        for (int i = 0; i < board.getBoardSize() - 1; i++) {
+            for (int j = 0; j < board.getBoardSize() - 1; j++) {
                 Tile currentTile = tiles.get(i).get(j);
                 boolean validBox = !Tile.E.equals(currentTile)
                         && currentTile.equals(tiles.get(i + 1).get(j))
